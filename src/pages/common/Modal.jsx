ec003e4efb97ed2d6e45b03a9487f97f9cc7394d
Grigory <barkev2009@bk.ru>
2022-03-04 22:34:47 +0300

    added modal to common

---
+++ src/pages/common/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/modal.css'
+
+const Modal = ({active, setActive}) => {
+  return (
+    <div className='modal'>
+        <div className="modal__content">
+        </div>
+    </div>
+  )
+}
+
+export default Modal
