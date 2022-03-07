6e39f53830a485a135305ed375723007e49c278f
Grigory <barkev2009@bk.ru>
2022-03-07 05:58:58 +0300

    finished constants

--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const ACCEPT_COMMENT = 'Спасибо за участие!';
-export const REJECT_COMMENT = 'К сожалению, фото не соответствует требованиям заказчика';
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const ACCEPT_COMMENT : string = 'Спасибо за участие!';
+export const REJECT_COMMENT : string = 'К сожалению, фото не соответствует требованиям заказчика';
