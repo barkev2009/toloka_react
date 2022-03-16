ca7efef66d2952cf8216f2d69ac4ead76428c94c
Grigory <barkev2009@bk.ru>
2022-03-16 23:53:09 +0300

    cleaned up the code

--- src/pages/main/components/SpinnerButtons/SpinnerImgDownloadButton.tsx
+++ src/pages/main/components/SpinnerButtons/SpinnerImgDownloadButton.tsx
@@ -7,56 +7,39 @@ import { RootState } from '../../../..'
 import { hideSpinner, showSpinner } from '../../../../redux/actions/appActions'
 import { setActivePool } from '../../../../redux/actions/poolActions'
 
-const SpinnerImgDownloadButton = ({poolID}) => {
+const SpinnerImgDownloadButton = ({ poolID }) => {
 
-    const token = useSelector<RootState>(state => state.token.yaToken)
-    const sandbox = useSelector<RootState>(state => state.sandbox.sandboxOn)
-    const poolImages : any = useSelector<RootState>(state => state.images.images.filter(item => item.details.pool_id === poolID))
-    const imagesAvailable = poolImages !== undefined ? poolImages.filter(item => item.status === 'SUBMITTED').length : 0
-    const loading = useSelector<RootState>(state => state.app.spinners[`img_${poolID}`])
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-        
-    const downloadImage = (sandbox, token, file_id, file_name) => {
-        axios({
-          method: 'GET',
-          url: 'http://127.0.0.1:8000/download_image/',
-          params : {
-            sandbox,
-            token,
-            file_id,
-            file_name
-          },
-        })
+  const token = useSelector<RootState>(state => state.token.yaToken)
+  const sandbox = useSelector<RootState>(state => state.sandbox.sandboxOn)
+  const poolImages: any = useSelector<RootState>(state => state.images.images.filter(item => item.details.pool_id === poolID))
+  const imagesAvailable = poolImages !== undefined ? poolImages.filter(item => item.status === 'SUBMITTED').length : 0
+  const loading = useSelector<RootState>(state => state.app.spinners[`img_${poolID}`])
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const downloadImages = async () => {
+    dispatch(showSpinner(`img_${poolID}`))
+
+    await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/download_images/',
+      data: {
+        sandbox,
+        token,
+        imageData: poolImages
       }
+    })
 
-    const downloadImages = async () => {
-        dispatch(showSpinner(`img_${poolID}`))
-        // poolImages.forEach(img => {
-        //   downloadImage(sandbox, token, img.id, img.fake_name);
-        // });
-
-        await axios({
-          method: 'POST',
-          url: 'http://127.0.0.1:8000/download_images/',
-          data: {
-            sandbox,
-            token,
-            imageData: poolImages
-          }
-        })
-
-        dispatch(setActivePool(poolID));
-
-        dispatch(hideSpinner(`img_${poolID}`));
-        navigate('/images', {replace: true});
-      }
-    return (
-        <button type="button" className='btn btn-warning' onClick={downloadImages} disabled={loading ? true : false}>
-            {loading ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> : ''}
-            {loading ? ' Loading...' : imagesAvailable + ' images available'}
-        </button>
-    )
+    dispatch(setActivePool(poolID));
+    dispatch(hideSpinner(`img_${poolID}`));
+    navigate('/images', { replace: true });
+  }
+  return (
+    <button type="button" className='btn btn-warning' onClick={downloadImages} disabled={loading ? true : false}>
+      {loading ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> : ''}
+      {loading ? ' Loading...' : imagesAvailable + ' images available'}
+    </button>
+  )
 }
 
 export default SpinnerImgDownloadButton
--- src/redux/reducers/tokenReducer.ts
+++ src/redux/reducers/tokenReducer.ts
@@ -1,24 +1,15 @@
 import { tokenState, tokenAction } from './../interfaces/tokenInterfaces';
-import { SET_YADISK_ID, SET_YADISK_URL, SET_YATOKEN, SET_YADISK_SECRET, SET_YADISK_TOKEN } from "../types";
+import { SET_YATOKEN, SET_YADISK_TOKEN } from "../types";
 
 const initialState : tokenState = {
     yaToken: localStorage.getItem('yaToken'),
-    yaDiskID: localStorage.getItem('yaDiskID'),
-    yaDiskSecret: localStorage.getItem('yaDiskSecret'),
-    yaDiskToken: localStorage.getItem('yaDiskToken'),
-    yaDiskUrl: null
+    yaDiskToken: localStorage.getItem('yaDiskToken')
 };
 
 export const tokenReducer = (state : tokenState = initialState, action : tokenAction) : tokenState => {
     switch (action.type) {
         case SET_YATOKEN:
             return {...state, yaToken: action.payload}
-        case SET_YADISK_URL:
-            return {...state, yaDiskUrl: action.payload}
-        case SET_YADISK_ID:
-            return {...state, yaDiskID: action.payload}
-        case SET_YADISK_SECRET:
-            return {...state, yaDiskSecret: action.payload}
         case SET_YADISK_TOKEN:
             return {...state, yaDiskToken: action.payload}
         default:
