2992c7c4a91d697ccd37870355e77ab0f4e91dc8
Grigory <barkev2009@bk.ru>
2022-03-19 16:46:30 +0300

    added resetting polygon token, fixed styling

--- src/pages/imagesPage/components/ModalContent.tsx
+++ src/pages/imagesPage/components/ModalContent.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import SpinnerSendTasksButton from '../../main/components/SpinnerButtons/SpinnerSendTasksButton'
 import { useDispatch, useSelector } from 'react-redux'
 import { sendCheckedTasks } from '../../../redux/actions/imageActions'
+import { setYaDiskToken } from '../../../redux/actions/tokenActions'
 import { RootState } from '../../../index'
 import GetPoligonToken from './GetPoligonToken'
 
@@ -16,34 +17,44 @@ const ModalContent = ({ images }) => {
 
   const sendTasks = () => {
     dispatch(sendCheckedTasks(sandbox, token, images, pushToDisk, yaDiskToken, 'Data from Toloka Pools'))
-    
+
   }
 
   return (
     <div className='change-buttons'>
-      <div>
+      <div className='container'>
         <SpinnerSendTasksButton
           onClick={sendTasks}
           disabled={images.length === 0 || images.filter(img => img.comment !== undefined && img.comment.trim() !== '').length < images.length}
         />
       </div>
-      {
-        yaDiskToken.toString() === 'null' ?
-          <GetPoligonToken /> :
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              checked={pushToDisk}
-              id="flexCheckDefault"
-              onChange={() => setPushToDisk(pushToDisk ? false : true)}
-              disabled={images.length === 0 || images.filter(img => img.comment !== undefined && img.comment.trim() !== '').length < images.length || !yaDiskToken}
-            />
-            <label className="form-check-label" htmlFor="flexCheckDefault">
-              Push accepted images to Yandex.Disk
-            </label>
-          </div>
-      }
+      <div className="container">
+        {
+          yaDiskToken.toString() === 'null' || yaDiskToken.toString() === '' ?
+            <GetPoligonToken /> :
+            <div className='container'>
+              <div className="container text-align-center">
+                <div className="form-check mb-5 w-max m0auto">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    checked={pushToDisk}
+                    id="flexCheckDefault"
+                    onChange={() => setPushToDisk(pushToDisk ? false : true)}
+                    disabled={images.length === 0 || images.filter(img => img.comment !== undefined && img.comment.trim() !== '').length < images.length || !yaDiskToken}
+                  />
+                  <label className="form-check-label" htmlFor="flexCheckDefault">
+                    Push accepted images to Yandex.Disk
+                  </label>
+                </div>
+              </div>
+              <h6 className='text-align-center'>{`Current Yandex poligon Token: ${yaDiskToken}`}</h6>
+              <div className="container text-align-center">
+                <button className='btn btn-outline-danger' onClick={() => dispatch(setYaDiskToken('null'))}>Reset Yandex Poligon Token</button>
+              </div>
+            </div>
+        }
+      </div>
     </div>
   )
 }
